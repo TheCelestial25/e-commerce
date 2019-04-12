dbc6a2b93ac848e10f50229b394272ffbd06c896
theCelestial25 <spriyanshu25@gmail.com>
2019-04-12 11:02:59 +0530

    Modify context.

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {storeProducts, detailProduct} from './data';
 
 
 const ProductContext = React.createContext();
@@ -6,9 +7,25 @@ const ProductContext = React.createContext();
 //Consumer
 
 class ProductProvider extends Component {
+  state = {
+    products:storeProducts,
+    detailProduct:detailProduct
+  }
+  handleDetail = () => {
+    console.log("hello from detail");
+  }
+  addToCart = () => {
+    console.log("hello from addToCart");
+  }
   render() {
     return (
-      <ProductContext.Provider value="hello from context">
+      <ProductContext.Provider value="hello from context"
+      value = {{
+...this.state,
+handleDetail: this.handleDetail,
+addToCart: this.addToCart
+      }}
+      >
         {this.props.children}
       </ ProductContext.Provider>
     )
