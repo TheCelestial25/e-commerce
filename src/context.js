5b4b53d00a374e371ed424cb6dc525d761332a75
theCelestial25 <spriyanshu25@gmail.com>
2019-04-15 08:02:25 +0530

    Add cart and set up its UI.

--- src/context.js
+++ src/context.js
@@ -10,10 +10,14 @@ class ProductProvider extends Component {
   state = {
     products:[],
     detailProduct:detailProduct,
-    cart:[],
+    cart:storeProducts,
     modalOpen:false,
     modalProduct:detailProduct,
-  }
+    cartSubTotal:0,
+    cartTax:0,
+    cartTotal: 0,
+  };
+
   componentDidMount() {
     this.setProducts();
   }
@@ -66,6 +70,18 @@ closeModal = () => {
     return {modalOpen:false}
   })
 }
+increment = (id) => {
+  console.log('this is increment method');
+}
+decrement = (id) => {
+  console.log('this is decrement method');
+}
+removeItem = (id) => {
+  console.log('item removed');
+}
+clearCart = () => {
+  console.log('cart was cleared');
+}
   render() {
     return (
       <ProductContext.Provider value="hello from context"
@@ -74,7 +90,11 @@ closeModal = () => {
 handleDetail: this.handleDetail,
 addToCart: this.addToCart,
 openModal:this.openModal,
-closeModal:this.closeModal
+closeModal:this.closeModal,
+increment:this.increment,
+decrement:this.decrement,
+removeItem:this.removeItem,
+clearCart:this.clearCart,
       }}
       >
         {this.props.children}
